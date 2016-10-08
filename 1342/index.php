7f51997360b749f74068c2d146ad20347d29a026
wangfei1234 <1425302858@qq.com>
2016-10-08 16:35:36 +0800

    no message

--- 1342/index.php
+++ 1342/index.php
@@ -1,10 +1,6 @@
 <?php
 phpinfo();
 print_r(phpversion());
-echo "123456";
-<<<<<<< HEAD
+echo "123456"; 
 echo "ABC";
 echo "12345巴拉巴拉";
-=======
-echo "ABC";
->>>>>>> 3458eedb346f8d9815761e074ea99d0fbc9bf75b
