24ad25c878cb73d0ac165be3fa6dd8b5606d6c5b
liaofuxi <liaofuxi@gmail.com>
2016-10-08 16:04:49 +0800

    12345

--- 1342/index.php
+++ 1342/index.php
@@ -2,9 +2,10 @@
 phpinfo();
 print_r(phpversion());
 echo "123456WRERT";
-<<<<<<< HEAD
+
 echo "23456yuio0-=";
 echo "1111111111112222222";
-=======
+
 echo "23456yuio0-=DSAEFE";
->>>>>>> fdd59efd740bd5bd7efbdd54b74c0292dbbbf4d3
+
+echo "12345";
