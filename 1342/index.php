0075c9b068c06e38be291fc732a237c3d49d40a0
liaofuxi <liaofuxi@gmail.com>
2016-10-08 16:34:17 +0800

    12345

--- 1342/index.php
+++ 1342/index.php
@@ -1,15 +1,3 @@
 <?php
 phpinfo();
 print_r(phpversion());
-echo "123456";
-<<<<<<< HEAD
-echo "ABC";
-echo "ZZZZZZZZZ";
-=======
-<<<<<<< HEAD
-echo "ABC";
-echo "12345巴拉巴拉";
-=======
-echo "ABC";
->>>>>>> 3458eedb346f8d9815761e074ea99d0fbc9bf75b
->>>>>>> 54525d13525b2f28b0a0ace948b1e20c0a57ee6b
