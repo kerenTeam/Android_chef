6f55d4cf74ef1a1fe9437dddc290787ee2375002
liaofuxi <liaofuxi@gmail.com>
2016-10-08 16:18:16 +0800

    vfesvf

--- 1342/index.php
+++ 1342/index.php
@@ -1,27 +1,4 @@
 <?php
 phpinfo();
 print_r(phpversion());
-echo "123456WRERT";
-
-echo "23456yuio0-=";
-echo "1111111111112222222";
-
-echo "23456yuio0-=DSAEFE";
-
-echo "12345";
-
-<<<<<<< HEAD
-
-
-echo "ABCD";
-
-echo "1234567890-=";
-=======
-<<<<<<< HEAD
-echo "abcd"
-=======
-
-
-echo "ABCD";
->>>>>>> bfed25d46828fe80032527f7f5bc5f119f1108cb
->>>>>>> f66de5da4fe05f63125a5a28aedd77495f20acc6
+echo "123456";
